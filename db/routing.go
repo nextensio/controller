02bd0ec9de0cae6c04bfe349e1eb19403e02e590
Gopa Kumar <gopakumarce@gmail.com>
2020-09-22 11:46:28 -0400

    Re #2: Add REST endpoints on controller for routing
    A route is a combination of a name like "www.google.com" and a tag like "v1" or "v2" etc..
    Add REST end points for add/del/get of routes, and add go unit test cases for the same

---
+++ db/routing.go
@@ -0,0 +1,82 @@
+package db
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// NOTE: The bson decoder will not work if the structure field names dont start with upper case
+type Route struct {
+	Route  string             `json:"route" bson:"_id"`
+	Tenant primitive.ObjectID `json:"tenant" bson:"tenant"`
+	Tag    string             `json:"tag" bson:"tag"`
+}
+
+// This API will add a new route or update a route if it already exists
+func DBAddRoute(data *Route) error {
+
+	if DBFindTenant(data.Tenant) == nil {
+		return fmt.Errorf("Cant find tenant %s", data.Tenant)
+	}
+
+	// The upsert option asks the DB to add a tenant if one is not found
+	upsert := true
+	after := options.After
+	opt := options.FindOneAndUpdateOptions{
+		ReturnDocument: &after,
+		Upsert:         &upsert,
+	}
+	err := routeCltn.FindOneAndUpdate(
+		context.TODO(),
+		bson.M{"_id": data.Route, "tenant": data.Tenant},
+		bson.D{
+			{"$set", bson.M{"_id": data.Route, "tenant": data.Tenant, "tag": data.Tag}},
+		},
+		&opt,
+	)
+
+	if err != nil {
+		return err.Err()
+	}
+	return nil
+}
+
+func DBFindRoute(tenant primitive.ObjectID, routeid string) *Route {
+	var route Route
+	err := routeCltn.FindOne(
+		context.TODO(),
+		bson.M{"_id": routeid, "tenant": tenant},
+	).Decode(&route)
+	if err != nil {
+		return nil
+	}
+	return &route
+}
+
+func DBFindAllRoutes(tenant primitive.ObjectID) []Route {
+	var routes []Route
+
+	cursor, err := routeCltn.Find(context.TODO(), bson.M{"tenant": tenant})
+	if err != nil {
+		return nil
+	}
+	err = cursor.All(context.TODO(), &routes)
+	if err != nil {
+		return nil
+	}
+
+	return routes
+}
+
+func DBDelRoute(tenant primitive.ObjectID, routeid string) error {
+	_, err := routeCltn.DeleteOne(
+		context.TODO(),
+		bson.M{"_id": routeid, "tenant": tenant},
+	)
+
+	return err
+}
---
+++ test/integ/route_test.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"nextensio/controller/db"
+	"nextensio/controller/router"
+	"testing"
+)
+
+type Route_v1 struct {
+	Route  string `json:"route" bson:"_id"`
+	Tenant string `json:"tenant" bson:"tenant"`
+	Tag    string `json:"tag" bson:"tag"`
+}
+
+func RouteAdd_v1(t *testing.T, addtenant bool, routeid string) {
+	if addtenant {
+		AddTenant_v1(t)
+	}
+	dbTenants := db.DBFindAllTenants()
+
+	route := Route_v1{
+		Route:  routeid,
+		Tenant: dbTenants[0].ID.Hex(),
+		Tag:    "version1",
+	}
+	body, err := json.Marshal(route)
+	if err != nil {
+		t.Error()
+		return
+	}
+	resp, err := http.Post("http://127.0.0.1:8080/api/v1/addroute", "application/json", bytes.NewBuffer(body))
+	if err != nil {
+		t.Error()
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Error()
+		return
+	}
+	var data router.AddrouteResult
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		t.Error()
+		return
+	}
+	if data.Result != "ok" {
+		t.Error()
+		return
+	}
+
+	dbTenant := db.DBFindRoute(dbTenants[0].ID, route.Route)
+	if dbTenant == nil {
+		t.Error()
+		return
+	}
+}
+
+func TestRouteAdd_v1(t *testing.T) {
+	db.DBReinit()
+	RouteAdd_v1(t, true, "www.google.com")
+}
+
+func TestRouteGet_v1(t *testing.T) {
+	db.DBReinit()
+	RouteAdd_v1(t, true, "www.google.com")
+	dbTenants := db.DBFindAllTenants()
+
+	resp, err := http.Get("http://127.0.0.1:8080/api/v1/getroute/" + dbTenants[0].ID.Hex() + "/www.google.com")
+	if err != nil {
+		t.Error()
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Error()
+		return
+	}
+	var data router.GetrouteResult
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		t.Error()
+		return
+	}
+	if data.Result != "ok" {
+		t.Error()
+		return
+	}
+	if string(data.Tag) != "version1" {
+		t.Error()
+		return
+	}
+}
+
+func TestGetAllRoutes_v1(t *testing.T) {
+	db.DBReinit()
+
+	RouteAdd_v1(t, true, "www.google.com")
+	RouteAdd_v1(t, false, "www.yahoo.com")
+
+	dbTenants := db.DBFindAllTenants()
+
+	resp, err := http.Get("http://127.0.0.1:8080/api/v1/getallroutes/" + dbTenants[0].ID.Hex())
+	if err != nil {
+		t.Error()
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Error()
+		return
+	}
+	var data []db.Route
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		t.Error()
+		return
+	}
+	if len(data) != 2 {
+		t.Error()
+		return
+	}
+	found := 0
+	for i := 0; i < len(data); i++ {
+		if data[i].Route == "www.google.com" {
+			found++
+		}
+		if data[i].Route == "www.yahoo.com" {
+			found++
+		}
+	}
+	if found != 2 {
+		t.Error()
+		return
+	}
+}
+
+func RouteDel_v1(t *testing.T, name string) {
+	dbTenants := db.DBFindAllTenants()
+
+	resp, err := http.Get("http://127.0.0.1:8080/api/v1/delroute/" + dbTenants[0].ID.Hex() + "/" + name)
+	if err != nil {
+		t.Error()
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Error()
+		return
+	}
+	var data router.DelrouteResult
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		t.Error()
+		return
+	}
+	if data.Result != "ok" {
+		t.Error()
+		return
+	}
+	if db.DBFindAllRoutes(dbTenants[0].ID) != nil {
+		t.Error()
+		return
+	}
+}
+
+func TestRouteDel_v1(t *testing.T) {
+	db.DBReinit()
+	RouteAdd_v1(t, true, "www.google.com")
+	RouteDel_v1(t, "www.google.com")
+}
