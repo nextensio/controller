d3f2631be926420a8d6480e0ff175b730143290a
Gopa Kumar <gopakumarce@gmail.com>
2022-01-31 20:42:58 -0500

    unify all client-ids to just one id

--- router/router_production.go
+++ router/router_production.go
@@ -1,3 +1,4 @@
+//go:build !unittest
 // +build !unittest
 
 package router
@@ -10,21 +11,9 @@ import (
 )
 
 func Authenticate(w http.ResponseWriter, r *http.Request) *context.Context {
-	// The Agents/Connectors and other UX/SDK-users are kept as seperate applications
-	// in the IDP (okta), mainly because all of them have seperate redirect-urls in their
-	// configs. So we need to validate the token against one of either client ids
-	cidMobileAgent := utils.GetEnv("AGENT_MOB_CLIENT_ID", "none")
-	ctx := IsAuthenticated(r, cidMobileAgent)
-	if ctx == nil {
-		cidApi := utils.GetEnv("API_CLIENT_ID", "none")
-		ctx = IsAuthenticated(r, cidApi)
-		if ctx == nil {
-			cidSpaAgent := utils.GetEnv("AGENT_SPA_CLIENT_ID", "none")
-			ctx = IsAuthenticated(r, cidSpaAgent)
-		}
-	}
 
-	return ctx
+	cidApi := utils.GetEnv("API_CLIENT_ID", "none")
+	return IsAuthenticated(r, cidApi)
 }
 
 func IdpGetUserInfo(API string, TOKEN string, userid string) (string, string, string, error) {
